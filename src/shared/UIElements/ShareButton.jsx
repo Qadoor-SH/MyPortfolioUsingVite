0312df57ca8e97912a37f29628d155c4b00832df
Qadoor-SH <124529771+Qadoor-SH@users.noreply.github.com>
2024-10-11 21:08:37 +0300

    Update ShareButton.jsx

--- src/shared/UIElements/ShareButton.jsx
+++ src/shared/UIElements/ShareButton.jsx
@@ -64,9 +64,9 @@ const ShareButton = () => {
             </TwitterShareButton>
             <WhatsappShareButton
               title={pageMetaDetails.title[i18n.resolvedLanguage]}
-              separator={`${
+              separator={`\n ${
                 pageMetaDetails.description[i18n.resolvedLanguage]
-              } /n \n`}
+              } \n`}
               url={deployedWebUrl}
               lang={i18n.resolvedLanguage}
               dir={i18n.dir(i18n.resolvedLanguage)}
